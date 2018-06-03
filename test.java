b9e68c15e5a3715a9e698a2ff93881a914231d72
Sai Vanamala <saivanamala714@gmail.com>
2018-06-02 23:25:20 -0400

    commit 3

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class test {
 	public static void main(String[] argv){
      System.out.println("Java application to test Git integration with Jenkins");
      System.out.println("First commit is successfull");
+     System.out.println("Second commit is successfull");
 	}
 }
